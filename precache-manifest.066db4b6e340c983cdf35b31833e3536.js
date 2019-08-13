35d2443c6ec22028d33d65d7badf0825300afb9b
Huiling Wu <huwu@ucdavis.edu>
2019-08-12 17:21:09 -0700

    Updates

---
+++ precache-manifest.066db4b6e340c983cdf35b31833e3536.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f17c248910413e74c8cc345752a0fbd",
+    "url": "/Web-Sudoku/index.html"
+  },
+  {
+    "revision": "93a83cce75e3b986a83a",
+    "url": "/Web-Sudoku/static/css/main.968144cf.chunk.css"
+  },
+  {
+    "revision": "8e13c07b311f922384dc",
+    "url": "/Web-Sudoku/static/js/2.895bb0b1.chunk.js"
+  },
+  {
+    "revision": "93a83cce75e3b986a83a",
+    "url": "/Web-Sudoku/static/js/main.bb08366f.chunk.js"
+  },
+  {
+    "revision": "2308db037eb0427ca6c3",
+    "url": "/Web-Sudoku/static/js/runtime~main.9c89b8f7.js"
+  }
+]);
