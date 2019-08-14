cbc5b9d4cfa74a7a32e1185df18f20ffdf5119cc
Huiling Wu <huwu@ucdavis.edu>
2019-08-13 21:28:10 -0700

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default ()=>{
+    return (
+        <div id='footer'>
+            Web Sudoku &copy;2019&nbsp;
+            <a title="Github" href="https://github.com/huwu55/Web-Sudoku" target="_blank">
+                Huiling Wu <i className="fab fa-github"></i>
+            </a>
+        </div>
+    );
+}
